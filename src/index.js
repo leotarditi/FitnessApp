24c45b231abb4fafc8fa0fe027d5493c3c2adb3f
Leo Damian Tarditi Pellegrino <57666013+leotarditi@users.noreply.github.com>
2020-11-11 13:58:02 -0300

    State
    Agrego un state a la imagen para probar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// const element = document.createElement('h1')
+// element.innerText = 'Hello React..'
+// const container = document.getElementById('root')
+// container.appendChild(element)
+// https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png
+//https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
-import exerciseImg from './images/exercise.png'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
@@ -10,7 +15,7 @@ const container = document.getElementById('root')
 ReactDOM.render(<Card 
                     title="Technique Guides"
                     description="Learn amazing street workout and calisthenics"
-                    img={exerciseImg}
+                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
                     leftColor="#A74CF2"
                     rightColor="#617BFB"
                 />, container)
