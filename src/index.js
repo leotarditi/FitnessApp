7a99f635f053149412ea0a8c9a2a3331a14bfb66
Leo Damian Tarditi Pellegrino <57666013+leotarditi@users.noreply.github.com>
2020-11-11 13:23:33 -0300

    Props
    Se agregaron props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
+import exerciseImg from './images/exercise.png'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
 // ReactDOM.render(__QUE__, __DONDE__)
-ReactDOM.render(<Card />, container)
+ReactDOM.render(<Card 
+                    title="Technique Guides"
+                    description="Learn amazing street workout and calisthenics"
+                    img={exerciseImg}
+                    leftColor="#A74CF2"
+                    rightColor="#617BFB"
+                />, container)
