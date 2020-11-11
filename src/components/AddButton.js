3d871b45d1f4e4b0eb061b301e52cd001de4468d
Leo Damian Tarditi Pellegrino <57666013+leotarditi@users.noreply.github.com>
2020-11-11 15:35:08 -0300

    ReactRouter
    Agrego reactrouter a la aplicacion (para ver las cartas agregar /exercise)

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import './styles/AddButton.css'
 import buttomImg from '../images/add.png'
+import { Link } from 'react-router-dom'
 
-function AddButton(props){
-    return (
-        <a href="/exercise/new">
-            <img src={buttomImg} class="Fitness-Add" />
-        </a>
-        
-    )
-}
+const AddButton = () => (
+    <Link to="/exercise/new">
+        <img src={buttomImg} className="Fitness-Add" alt="exercise"/>
+    </Link>
+)
 
 export default AddButton
